bcad1ef891f3cf92a181ccc61416c17d08762bf7
User <user@Users-MacBook-Air.local>
2022-04-15 15:25:54 +0100

    setting up our in memory databse

--- src/main/java/com/example/reddit_clone/repository/LinkRepository.java
+++ src/main/java/com/example/reddit_clone/repository/LinkRepository.java
@@ -4,4 +4,5 @@ import com.example.reddit_clone.entity.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+    Link findByTitle(String title);
 }
